a6b532163a71c9d1d28f6bfddf99fa235a03a053
thesun4sky <thesun4sky@gmail.com>
2016-10-18 17:58:06 +0900

    OpenGLES 2.0 버전 Shader 3D 텍스쳐 매핑 적용

--- app/src/main/java/com/example/teasunkim/bighead_gles3/MainActivity.java
+++ app/src/main/java/com/example/teasunkim/bighead_gles3/MainActivity.java
@@ -1,35 +1,60 @@
 package com.example.teasunkim.bighead_gles3;
 
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    // 렌더링 결과를 그릴 OpenGL view를 선언.
-    private GLSurfaceView mGLView;
+public class MainActivity extends Activity
+{
+    /** Hold a reference to our GLSurfaceView */
+    private GLSurfaceView mGLSurfaceView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
-        // GLES30SurfaceView 라는 클래스를 이용해서 GLSurfaceView를 초기화 할 예정.
-        mGLView = new GLES30SurfaceView(this);
-        setContentView(mGLView);    // mGLView 객체로 contentView를 설정해준다.
+        mGLSurfaceView = new GLSurfaceView(this);
+
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (supportsEs2)
+        {
+            // Request an OpenGL ES 2.0 compatible context.
+            mGLSurfaceView.setEGLContextClientVersion(2);
+
+            // Set the renderer to our demo renderer, defined below.
+            mGLSurfaceView.setRenderer(new OpenGLES20Renderer(this));
+        }
+        else
+        {
+            // This is where you could create an OpenGL ES 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            return;
+        }
+
+        setContentView(mGLSurfaceView);
     }
-}
 
-class GLES30SurfaceView extends GLSurfaceView {
-    private final GLES30Renderer mRenderer;
+    @Override
+    protected void onResume()
+    {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
 
-    public GLES30SurfaceView(Context context){
-        super(context);
-
-        // OpenGL ES 3.0 context를 생성.
-        setEGLContextClientVersion(3);
-        mRenderer = new GLES30Renderer();
-
-        // GLSurfaceView에 렌더링 결과를 그리기 위해 현재 SurfaceView에 렌더러를 설정해준다.
-        setRenderer(mRenderer);
+    @Override
+    protected void onPause()
+    {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause();
+        mGLSurfaceView.onPause();
     }
 }
---
+++ app/src/main/java/com/example/teasunkim/bighead_gles3/util/LoggerConfig.java
@@ -0,0 +1,5 @@
+package com.example.teasunkim.bighead_gles3.util;
+
+public class LoggerConfig {
+	public static final boolean ON = true;
+}
